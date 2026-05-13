ea37ff4cffc09f820dd178af51bfb4731537435d
Hoang Trang <trang010280@gmail.com>
2026-05-13 11:36:33 +0700

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@/components/ui/typography";
+
+export default function Home() {
+  return (
+    <main className="p-10 space-y-8 max-w-4xl mx-auto">
+      <Typography variant="h1">Home Page</Typography>
+    </main>
+  );
+}
